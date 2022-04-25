8d5f2a1f07e4b648315a6d3577977017d4592d40
Zakaria Lounes <zakaria.lounes@gmail.com>
2022-04-26 01:59:13 +0200

    ADD setup_cosmovisor.sh script. The easiest and fatest way to configure and install Cosmovisor

---
+++ README.md
@@ -0,0 +1,12 @@
+# EZ Staking Utilities
+
+### [setup_cosmovisor.sh](setup_cosmovisor.sh)
+````shell
+The easiest and fastest way to configure and install Cosmovisor automatically.
+
+Syntax: bash <(curl -Lfs https://raw.githubusercontent.com/EZStaking/ez_utilities/main/setup_cosmovisor.sh) [OPTIONS] [DAEMON_NAME] [DAEMON_HOME]
+Syntax: ./setup_cosmovisor.sh [OPTIONS] [DAEMON_NAME] [DAEMON_HOME]
+
+options:
+  -h     Print this help and exit
+````
---
+++ setup_cosmovisor.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+help()
+{
+   echo "The easiest and fastest way to configure and install Cosmovisor automatically."
+   echo
+   echo "Syntax: bash <(curl -Lfs https://raw.githubusercontent.com/EZStaking/ez_utilities/main/setup_cosmovisor.sh) [OPTIONS] [DAEMON_NAME] [DAEMON_HOME]"
+   echo "Syntax: ./setup_cosmovisor.sh [OPTIONS] [DAEMON_NAME] [DAEMON_HOME]"
+   echo
+   echo "options:"
+   echo -e "  -h\t Print this help and exit"
+}
+
+while getopts ":h" option; do
+   case $option in
+      h)
+         help
+         exit;;
+     \?)
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+if [[ -z $1 || -z $2 ]]; then
+  help
+  exit
+fi
+
+DAEMON_NAME=$1
+DAEMON_HOME=$2
+
+installCosmovisor() {
+  go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0
+}
+
+setupCosmovisor() {
+  echo "export DAEMON_NAME=$DAEMON_NAME" >> $HOME/.profile
+  echo "export DAEMON_HOME=$DAEMON_HOME" >> $HOME/.profile
+  echo "Cosmovisor configuration added in the .profile file."
+
+  source $HOME/.profile
+
+  mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
+  mkdir -p $DAEMON_HOME/cosmovisor/upgrades
+  echo "Cosmovisor directories created."
+
+  cp $HOME/go/bin/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
+  echo "Cosmovisor binary copied."
+  echo ""
+}
+
+# @todo: update AND/OR create the service automatically
+updateCosmovisorService() {
+  echo "Cosmovisor service to update:"
+
+  cat <<EndOfMessage
+Environment="DAEMON_NAME=$DAEMON_NAME"
+Environment="DAEMON_HOME=$DAEMON_HOME"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="DAEMON_LOG_BUFFER_SIZE=512"
+EndOfMessage
+}
+
+### MAIN
+installCosmovisor
+setupCosmovisor
+updateCosmovisorService
