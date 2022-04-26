a650bdf6718f2e4af42e88ce4c88e98fc7236f63
Zakaria Lounes <zakaria.lounes@gmail.com>
2022-04-26 15:09:59 +0200

    ADD run_script.sh script. The easiest and fatest way to run a script directly from Github

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # EZ Staking Utilities
 
+### [run_script.sh](run_script.sh)
+````shell
+The easiest and fastest way to run a script directly from Github.
+
+Syntax: bash <(curl -Ls https://raw.githubusercontent.com/EZStaking/ez_utilities/main/run_script.sh) [OPTIONS] [FILE]
+Syntax: ./run_script.sh [OPTIONS] [FILE]
+
+options:
+  -h                Print this help and exit
+  -u EZStaking      Github User
+                      EZStaking (default)
+  -r ez_utilities   Github Repository
+                      ez_utilities (default)
+  -b main           Github branch
+                      main (default)
+````
+
 ### [setup_cosmovisor.sh](setup_cosmovisor.sh)
 ````shell
 The easiest and fastest way to configure and install Cosmovisor automatically.
---
+++ run_script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+help()
+{
+   echo "The easiest and fastest way to run a script directly from Github."
+   echo
+   echo "Syntax: bash <(curl -Lfs https://raw.githubusercontent.com/EZStaking/ez_utilities/main/run_script.sh) [OPTIONS] [FILE]"
+   echo "Syntax: ./run_script.sh [OPTIONS] [FILE]"
+   echo
+   echo "options:"
+   echo -e "  -h\t\t Print this help and exit"
+
+   echo -e "  -u EZStaking\t Github User"
+   echo -e "  \t\t   EZStaking (default)"
+
+   echo -e "  -r ez_utilities\t Github Repository"
+   echo -e "  \t\t   ez_utilities (default)"
+
+   echo -e "  -b main\t Github branch"
+   echo -e "  \t\t   main (default)"
+}
+
+while getopts ":h:u:r:b:" option; do
+   case $option in
+      h)
+         help
+         exit;;
+      u)
+        USER_REPOSITORY=$OPTARG
+        ;;
+      r)
+        REPOSITORY=$OPTARG
+        ;;
+      b)
+        BRANCH=$OPTARG
+        ;;
+     \?)
+       echo "Error: Invalid option"
+       exit;;
+   esac
+done
+
+if [[ -z $1 ]]; then
+  help
+  exit
+fi
+
+FILE=$1
+
+if [[ -z $USER_REPOSITORY ]]; then
+  USER_REPOSITORY=EZStaking
+fi
+
+if [[ -z $REPOSITORY ]]; then
+  REPOSITORY=ez_utilities
+fi
+
+if [[ -z $BRANCH ]]; then
+  BRANCH=main
+fi
+
+bash <(curl -Lfs https://raw.githubusercontent.com/$USER_REPOSITORY/$REPOSITORY/$BRANCH/$FILE)
